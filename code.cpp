c005b8dd8813d6321e4d29566d929bfe34d0b611
snehanixon <nixonjohnyt@gmail.com>
2026-07-03 10:41:45 +0530

    Refactor parking system code with ultrasonic sensor

--- code.cpp
+++ code.cpp
@@ -1,52 +1,63 @@
-#include <Wire.h> 
-#include <LiquidCrystal_I2C.h>
-LiquidCrystal_I2C lcd(0x27,16,2);   
-#include <Servo.h> 
-Servo myservo;
-int IR1 = 2;
-int IR2 = 3;
-int Slot = 4;           //Total number of parking Slots
-int flag1 = 0;
-int flag2 = 0;
-void setup() {
+// Eazytronic.com
+#include<Servo.h>
+int IR_sensor = 0;
+Servo s1;
+const int trigPin = 5;
+const int echoPin = 6;
+long duration;
+int distanceCm, distanceInch;
+void setup()
+{
+
   Serial.begin(9600); 
-    lcd.init(); //initialize the lcd
-    lcd.backlight(); //open the backlight
-pinMode(IR1, INPUT);
-pinMode(IR2, INPUT);
-myservo.attach(4);
-myservo.write(100);
-lcd.setCursor (0,0);
-lcd.print("     ARDUINO    ");
-lcd.setCursor (0,1);
-lcd.print(" PARKING SYSTEM ");
-delay (2000);
-lcd.clear();  
+  s1.attach(7);
+  pinMode(4,INPUT);   // Soil moisture Sensor Pin
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+
 }
-void loop(){ 
-if(digitalRead (IR1) == LOW && flag1==0){
-if(Slot>0){flag1=1;
-if(flag2==0){myservo.write(0); Slot = Slot-1;}
-}else{
-lcd.setCursor (0,0);
-lcd.print("    SORRY :(    ");  
-lcd.setCursor (0,1);
-lcd.print("  Parking Full  "); 
-delay (3000);
-lcd.clear(); 
+void loop() 
+{
+digitalWrite(trigPin, LOW);
+delayMicroseconds(2);
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin, LOW);
+duration = pulseIn(echoPin, HIGH);
+distanceCm= duration*0.034/2;
+distanceInch = duration*0.0133/2;
+  IR_sensor = digitalRead(4);
+  Serial.println(IR_sensor);
+
+  Serial.println("Distance cm: ");
+  Serial.println(distanceCm);
+  delay(100);
+
+if(distanceCm <20)
+{
+  
+    if (IR_sensor == 0)
+  {
+       delay(1000);
+       s1.write(100);
+       delay(1000);
+       
+  }
+
+      if (IR_sensor == 1)
+  {
+       delay(1000);
+       s1.write(0);
+       delay(1000);
+       
+  }
+
 }
+
+else
+{ 
+    s1.write(50);
+  delay(1000);
+
 }
-if(digitalRead (IR2) == LOW && flag2==0){flag2=1;
-if(flag1==0){myservo.write(0); Slot = Slot+1;}
-}
-if(flag1==1 && flag2==1){
-delay (1000);
-myservo.write(100);
-flag1=0, flag2=0;
-}
-lcd.setCursor (0,0);
-lcd.print("    WELCOME!    ");
-lcd.setCursor (0,1);
-lcd.print("Slot Left: ");
-lcd.print(Slot);
 }
